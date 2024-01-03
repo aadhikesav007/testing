d2ad8f0aef0c4da3fecbab0570c944ad2f4adb0c
aadhikesav007 <hackathonkrce@gmail.com>
2024-01-03 07:00:00 +0000

    Refactor: implement logic module 7

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2024-01-03T06:00:00+00:00 - Task ID: 6
-const validateEmail = (email) => /^[^s@]+@[^s@]+.[^s@]+$/.test(email);
+// Update for 2024-01-03T07:00:00+00:00 - Task ID: 7
+const onHandleSubmit = (e) => { e.preventDefault(); console.log('Form Submitted'); };
 
 export default {};
