6948f39ae6dbee43ecf114e67ab68943fa6e3b7b
aadhikesav007 <hackathonkrce@gmail.com>
2024-01-17 06:00:00 +0000

    Refactor: implement logic module 6

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2024-01-17T05:00:00+00:00 - Task ID: 5
-const validateEmail = (email) => /^[^s@]+@[^s@]+.[^s@]+$/.test(email);
+// Update for 2024-01-17T06:00:00+00:00 - Task ID: 6
+const getLocalStorage = (key) => JSON.parse(localStorage.getItem(key));
 
 export default {};
