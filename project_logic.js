e4206dc206f14431bfe4b0964dfd98ffaf75eceb
aadhikesav007 <hackathonkrce@gmail.com>
2024-01-24 01:00:00 +0000

    Refactor: implement logic module 1

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2024-01-24T00:00:00+00:00 - Task ID: 0
-const formatCurrency = (amount) => new Intl.NumberFormat('en-IN').format(amount);
+// Update for 2024-01-24T01:00:00+00:00 - Task ID: 1
+const fetchData = async (url) => { const res = await fetch(url); return res.json(); };
 
 export default {};
