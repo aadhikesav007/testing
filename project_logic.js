12872c7fe408f24a4b08a52d1219c05bd5748111
aadhikesav007 <hackathonkrce@gmail.com>
2024-01-25 09:00:00 +0000

    Refactor: implement logic module 9

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2024-01-25T08:00:00+00:00 - Task ID: 8
-const fetchData = async (url) => { const res = await fetch(url); return res.json(); };
+// Update for 2024-01-25T09:00:00+00:00 - Task ID: 9
+const toggleModal = () => setIsOpen(prev => !prev);
 
 export default {};
