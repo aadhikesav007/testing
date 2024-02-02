ddc90b6a4d45ee4d9c387c912527166803b54bb2
aadhikesav007 <hackathonkrce@gmail.com>
2024-02-02 09:00:00 +0000

    Refactor: implement logic module 9

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2024-02-02T08:00:00+00:00 - Task ID: 8
-const filterActiveUsers = (users) => users.filter(user => user.isActive);
+// Update for 2024-02-02T09:00:00+00:00 - Task ID: 9
+const debounce = (fn, ms) => { let timeout; return () => { clearTimeout(timeout); timeout = setTimeout(fn, ms); }; };
 
 export default {};
