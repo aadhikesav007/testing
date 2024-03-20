113037a828ce583a53b31b70c6c04fc9fd55f164
aadhikesav007 <hackathonkrce@gmail.com>
2024-03-20 02:00:00 +0000

    Refactor: implement logic module 2

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2024-03-20T01:00:00+00:00 - Task ID: 1
-const toggleModal = () => setIsOpen(prev => !prev);
+// Update for 2024-03-20T02:00:00+00:00 - Task ID: 2
+const getLocalStorage = (key) => JSON.parse(localStorage.getItem(key));
 
 export default {};
