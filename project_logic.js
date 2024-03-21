402c3f518a846b035c0312cad96c7b32322e8a30
aadhikesav007 <hackathonkrce@gmail.com>
2024-03-21 06:00:00 +0000

    Refactor: implement logic module 6

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2024-03-21T05:00:00+00:00 - Task ID: 5
+// Update for 2024-03-21T06:00:00+00:00 - Task ID: 6
 const debounce = (fn, ms) => { let timeout; return () => { clearTimeout(timeout); timeout = setTimeout(fn, ms); }; };
 
 export default {};
