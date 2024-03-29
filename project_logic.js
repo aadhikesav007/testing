03727f1f377ba9f05c610802eb1821842a002d11
aadhikesav007 <hackathonkrce@gmail.com>
2024-03-29 09:00:00 +0000

    Refactor: implement logic module 9

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2024-03-29T08:00:00+00:00 - Task ID: 8
-const validateEmail = (email) => /^[^s@]+@[^s@]+.[^s@]+$/.test(email);
+// Update for 2024-03-29T09:00:00+00:00 - Task ID: 9
+const filterActiveUsers = (users) => users.filter(user => user.isActive);
 
 export default {};
