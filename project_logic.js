e1afa2e4832e274c67e74d8ad39488e319ac5d42
aadhikesav007 <hackathonkrce@gmail.com>
2024-05-11 02:00:00 +0000

    Refactor: implement logic module 2

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2024-05-11T01:00:00+00:00 - Task ID: 1
+// Update for 2024-05-11T02:00:00+00:00 - Task ID: 2
 const validateEmail = (email) => /^[^s@]+@[^s@]+.[^s@]+$/.test(email);
 
 export default {};
