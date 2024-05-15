b41e5a0a5b415aebeb46ee673d33097711157e43
aadhikesav007 <hackathonkrce@gmail.com>
2024-05-15 08:00:00 +0000

    Refactor: implement logic module 8

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2024-05-15T07:00:00+00:00 - Task ID: 7
-const calculateTotal = (cart) => cart.reduce((acc, item) => acc + item.price, 0);
+// Update for 2024-05-15T08:00:00+00:00 - Task ID: 8
+const onHandleSubmit = (e) => { e.preventDefault(); console.log('Form Submitted'); };
 
 export default {};
