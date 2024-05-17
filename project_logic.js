d35a15e54ddfdaa73fee067c099916b0b694cfbe
aadhikesav007 <hackathonkrce@gmail.com>
2024-05-17 02:00:00 +0000

    Refactor: implement logic module 2

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2024-05-17T01:00:00+00:00 - Task ID: 1
-const onHandleSubmit = (e) => { e.preventDefault(); console.log('Form Submitted'); };
+// Update for 2024-05-17T02:00:00+00:00 - Task ID: 2
+const sortItems = (items) => items.sort((a, b) => a.value - b.value);
 
 export default {};
