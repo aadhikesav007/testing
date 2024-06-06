45e510498fedc8c4e99fedc715182d6d91de5fe8
aadhikesav007 <hackathonkrce@gmail.com>
2024-06-06 02:00:00 +0000

    Refactor: implement logic module 2

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2024-06-06T01:00:00+00:00 - Task ID: 1
-const getLocalStorage = (key) => JSON.parse(localStorage.getItem(key));
+// Update for 2024-06-06T02:00:00+00:00 - Task ID: 2
+const formatCurrency = (amount) => new Intl.NumberFormat('en-IN').format(amount);
 
 export default {};
