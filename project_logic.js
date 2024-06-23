4832268192addcba5d03c9e625da95ab2803d46b
aadhikesav007 <hackathonkrce@gmail.com>
2024-06-23 06:00:00 +0000

    Refactor: implement logic module 6

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2024-06-23T05:00:00+00:00 - Task ID: 5
+// Update for 2024-06-23T06:00:00+00:00 - Task ID: 6
 const filterActiveUsers = (users) => users.filter(user => user.isActive);
 
 export default {};
