abe65f8300cf27621f33e4ca30c4764c5418e5b3
aadhikesav007 <hackathonkrce@gmail.com>
2024-06-26 03:00:00 +0000

    Refactor: implement logic module 3

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2024-06-26T02:00:00+00:00 - Task ID: 2
-const calculateTotal = (cart) => cart.reduce((acc, item) => acc + item.price, 0);
+// Update for 2024-06-26T03:00:00+00:00 - Task ID: 3
+const fetchData = async (url) => { const res = await fetch(url); return res.json(); };
 
 export default {};
