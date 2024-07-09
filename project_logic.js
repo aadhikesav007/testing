47fe270dc3721ff21d189764e0fc65be1f8dfee1
aadhikesav007 <hackathonkrce@gmail.com>
2024-07-09 07:00:00 +0000

    Refactor: implement logic module 7

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2024-07-09T06:00:00+00:00 - Task ID: 6
-const toggleModal = () => setIsOpen(prev => !prev);
+// Update for 2024-07-09T07:00:00+00:00 - Task ID: 7
+const onHandleSubmit = (e) => { e.preventDefault(); console.log('Form Submitted'); };
 
 export default {};
