ca8fca24ee773f80d4d6aae010a5993c1e6f04c5
aadhikesav007 <hackathonkrce@gmail.com>
2024-07-22 05:00:00 +0000

    Refactor: implement logic module 5

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2024-07-22T04:00:00+00:00 - Task ID: 4
-const sortItems = (items) => items.sort((a, b) => a.value - b.value);
+// Update for 2024-07-22T05:00:00+00:00 - Task ID: 5
+const getLocalStorage = (key) => JSON.parse(localStorage.getItem(key));
 
 export default {};
