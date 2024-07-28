cdfdc73fbbf8213fa1bd762248867d05a66cda9e
aadhikesav007 <hackathonkrce@gmail.com>
2024-07-28 03:00:00 +0000

    Refactor: implement logic module 3

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2024-07-28T02:00:00+00:00 - Task ID: 2
-const sortItems = (items) => items.sort((a, b) => a.value - b.value);
+// Update for 2024-07-28T03:00:00+00:00 - Task ID: 3
+const filterActiveUsers = (users) => users.filter(user => user.isActive);
 
 export default {};
