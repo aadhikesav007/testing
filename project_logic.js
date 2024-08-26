c0e3d814b60d898ad77126b78240fa10c1a7e6b9
aadhikesav007 <hackathonkrce@gmail.com>
2024-08-26 00:00:00 +0000

    Refactor: implement logic module 0

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2024-08-25T09:00:00+00:00 - Task ID: 9
-const fetchData = async (url) => { const res = await fetch(url); return res.json(); };
+// Update for 2024-08-26T00:00:00+00:00 - Task ID: 0
+const sortItems = (items) => items.sort((a, b) => a.value - b.value);
 
 export default {};
