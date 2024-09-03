418078c262529fe5aad3cb60e16c1e0259818105
aadhikesav007 <hackathonkrce@gmail.com>
2024-09-03 01:00:00 +0000

    Refactor: implement logic module 1

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2024-09-03T00:00:00+00:00 - Task ID: 0
-const debounce = (fn, ms) => { let timeout; return () => { clearTimeout(timeout); timeout = setTimeout(fn, ms); }; };
+// Update for 2024-09-03T01:00:00+00:00 - Task ID: 1
+const formatCurrency = (amount) => new Intl.NumberFormat('en-IN').format(amount);
 
 export default {};
