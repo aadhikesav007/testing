d5dc85ab475894a83d54fdfd923f586650990f6e
aadhikesav007 <hackathonkrce@gmail.com>
2024-10-11 09:00:00 +0000

    Refactor: implement logic module 9

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2024-10-11T08:00:00+00:00 - Task ID: 8
-const filterActiveUsers = (users) => users.filter(user => user.isActive);
+// Update for 2024-10-11T09:00:00+00:00 - Task ID: 9
+const formatCurrency = (amount) => new Intl.NumberFormat('en-IN').format(amount);
 
 export default {};
