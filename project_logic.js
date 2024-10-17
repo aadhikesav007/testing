fd43dc2eb7d947f4b3b4c66207ccf05c3aaedf5f
aadhikesav007 <hackathonkrce@gmail.com>
2024-10-17 04:00:00 +0000

    Refactor: implement logic module 4

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2024-10-17T03:00:00+00:00 - Task ID: 3
-const sortItems = (items) => items.sort((a, b) => a.value - b.value);
+// Update for 2024-10-17T04:00:00+00:00 - Task ID: 4
+const validateEmail = (email) => /^[^s@]+@[^s@]+.[^s@]+$/.test(email);
 
 export default {};
