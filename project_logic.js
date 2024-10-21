67fd477b3449d0ec2a0b7b8d72d82d8dfd834c5b
aadhikesav007 <hackathonkrce@gmail.com>
2024-10-21 02:00:00 +0000

    Refactor: implement logic module 2

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2024-10-21T01:00:00+00:00 - Task ID: 1
-const validateEmail = (email) => /^[^s@]+@[^s@]+.[^s@]+$/.test(email);
+// Update for 2024-10-21T02:00:00+00:00 - Task ID: 2
+const fetchData = async (url) => { const res = await fetch(url); return res.json(); };
 
 export default {};
