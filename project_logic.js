d5f173e99bd7a5f6915afb65824a8d2d845108a0
aadhikesav007 <hackathonkrce@gmail.com>
2024-11-23 05:00:00 +0000

    Refactor: implement logic module 5

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2024-11-23T04:00:00+00:00 - Task ID: 4
-const filterActiveUsers = (users) => users.filter(user => user.isActive);
+// Update for 2024-11-23T05:00:00+00:00 - Task ID: 5
+const getLocalStorage = (key) => JSON.parse(localStorage.getItem(key));
 
 export default {};
