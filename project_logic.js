7455fce2c2ae838e479298685197535c095483b6
aadhikesav007 <hackathonkrce@gmail.com>
2024-12-11 04:00:00 +0000

    Refactor: implement logic module 4

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2024-12-11T03:00:00+00:00 - Task ID: 3
-const filterActiveUsers = (users) => users.filter(user => user.isActive);
+// Update for 2024-12-11T04:00:00+00:00 - Task ID: 4
+const fetchData = async (url) => { const res = await fetch(url); return res.json(); };
 
 export default {};
