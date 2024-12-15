7d65ba18f11efac66006988095bbcd038468fdcb
aadhikesav007 <hackathonkrce@gmail.com>
2024-12-15 00:00:00 +0000

    Refactor: implement logic module 0

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2024-12-14T09:00:00+00:00 - Task ID: 9
-const filterActiveUsers = (users) => users.filter(user => user.isActive);
+// Update for 2024-12-15T00:00:00+00:00 - Task ID: 0
+const onHandleSubmit = (e) => { e.preventDefault(); console.log('Form Submitted'); };
 
 export default {};
