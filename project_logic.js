72381453816b7771d7f53b9588b8ed7a3a7a09d1
aadhikesav007 <hackathonkrce@gmail.com>
2024-12-21 05:00:00 +0000

    Refactor: implement logic module 5

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2024-12-21T04:00:00+00:00 - Task ID: 4
+// Update for 2024-12-21T05:00:00+00:00 - Task ID: 5
 const formatCurrency = (amount) => new Intl.NumberFormat('en-IN').format(amount);
 
 export default {};
