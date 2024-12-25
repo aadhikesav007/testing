ef7622498e734a1b5fc5f6ebbb42a258886538ee
aadhikesav007 <hackathonkrce@gmail.com>
2024-12-25 00:00:00 +0000

    Refactor: implement logic module 0

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2024-12-24T09:00:00+00:00 - Task ID: 9
-const calculateTotal = (cart) => cart.reduce((acc, item) => acc + item.price, 0);
+// Update for 2024-12-25T00:00:00+00:00 - Task ID: 0
+const sortItems = (items) => items.sort((a, b) => a.value - b.value);
 
 export default {};
