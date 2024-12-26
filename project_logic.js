bab47615c50f75d6e119e419cb4e797fce37a653
aadhikesav007 <hackathonkrce@gmail.com>
2024-12-26 00:00:00 +0000

    Refactor: implement logic module 0

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2024-12-25T09:00:00+00:00 - Task ID: 9
-const formatCurrency = (amount) => new Intl.NumberFormat('en-IN').format(amount);
+// Update for 2024-12-26T00:00:00+00:00 - Task ID: 0
+const calculateTotal = (cart) => cart.reduce((acc, item) => acc + item.price, 0);
 
 export default {};
