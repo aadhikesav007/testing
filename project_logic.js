a5cf8d62e5102fbb5468ea9fa4b01663b3602274
aadhikesav007 <hackathonkrce@gmail.com>
2024-12-30 09:00:00 +0000

    Refactor: implement logic module 9

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2024-12-30T08:00:00+00:00 - Task ID: 8
-const getLocalStorage = (key) => JSON.parse(localStorage.getItem(key));
+// Update for 2024-12-30T09:00:00+00:00 - Task ID: 9
+const calculateTotal = (cart) => cart.reduce((acc, item) => acc + item.price, 0);
 
 export default {};
