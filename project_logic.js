a2ba09238cd05d0248f594e53c22aa860ce81559
aadhikesav007 <hackathonkrce@gmail.com>
2025-01-04 06:00:00 +0000

    Refactor: implement logic module 6

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2025-01-04T05:00:00+00:00 - Task ID: 5
-const calculateTotal = (cart) => cart.reduce((acc, item) => acc + item.price, 0);
+// Update for 2025-01-04T06:00:00+00:00 - Task ID: 6
+const debounce = (fn, ms) => { let timeout; return () => { clearTimeout(timeout); timeout = setTimeout(fn, ms); }; };
 
 export default {};
