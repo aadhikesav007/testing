d712b82fc444f9fc2403f450b60e2e3104f3f0d1
aadhikesav007 <hackathonkrce@gmail.com>
2025-01-10 06:00:00 +0000

    Refactor: implement logic module 6

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2025-01-10T05:00:00+00:00 - Task ID: 5
-const toggleModal = () => setIsOpen(prev => !prev);
+// Update for 2025-01-10T06:00:00+00:00 - Task ID: 6
+const sortItems = (items) => items.sort((a, b) => a.value - b.value);
 
 export default {};
