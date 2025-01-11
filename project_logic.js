398a42b1e784ba76f0000b3afe6b6384d7d6261f
aadhikesav007 <hackathonkrce@gmail.com>
2025-01-11 03:00:00 +0000

    Refactor: implement logic module 3

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2025-01-11T02:00:00+00:00 - Task ID: 2
-const toggleModal = () => setIsOpen(prev => !prev);
+// Update for 2025-01-11T03:00:00+00:00 - Task ID: 3
+const filterActiveUsers = (users) => users.filter(user => user.isActive);
 
 export default {};
