41f11ff1ec4cc72b8207a62adec5133246c5c8fd
aadhikesav007 <hackathonkrce@gmail.com>
2025-01-16 01:00:00 +0000

    Refactor: implement logic module 1

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2025-01-16T00:00:00+00:00 - Task ID: 0
-const toggleModal = () => setIsOpen(prev => !prev);
+// Update for 2025-01-16T01:00:00+00:00 - Task ID: 1
+const validateEmail = (email) => /^[^s@]+@[^s@]+.[^s@]+$/.test(email);
 
 export default {};
