c5183c0e2638176a471543069e9f70a714e7ac57
aadhikesav007 <hackathonkrce@gmail.com>
2025-01-27 07:00:00 +0000

    Refactor: implement logic module 7

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2025-01-27T06:00:00+00:00 - Task ID: 6
+// Update for 2025-01-27T07:00:00+00:00 - Task ID: 7
 const getLocalStorage = (key) => JSON.parse(localStorage.getItem(key));
 
 export default {};
