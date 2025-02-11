497ccf4cc85fc4a3b66eb14c7798d03ec2a91ee6
aadhikesav007 <hackathonkrce@gmail.com>
2025-02-11 04:00:00 +0000

    Refactor: implement logic module 4

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2025-02-11T03:00:00+00:00 - Task ID: 3
-const validateEmail = (email) => /^[^s@]+@[^s@]+.[^s@]+$/.test(email);
+// Update for 2025-02-11T04:00:00+00:00 - Task ID: 4
+const calculateTotal = (cart) => cart.reduce((acc, item) => acc + item.price, 0);
 
 export default {};
