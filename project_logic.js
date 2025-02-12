6ae14be298160a5860a6d170b4992993f73a6577
aadhikesav007 <hackathonkrce@gmail.com>
2025-02-12 04:00:00 +0000

    Refactor: implement logic module 4

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2025-02-12T03:00:00+00:00 - Task ID: 3
-const onHandleSubmit = (e) => { e.preventDefault(); console.log('Form Submitted'); };
+// Update for 2025-02-12T04:00:00+00:00 - Task ID: 4
+const getLocalStorage = (key) => JSON.parse(localStorage.getItem(key));
 
 export default {};
