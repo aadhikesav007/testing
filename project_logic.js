3512ec0d5bb1583df0d7683ecada4c818d6a5727
aadhikesav007 <hackathonkrce@gmail.com>
2025-09-03 02:00:00 +0000

    Refactor: implement logic module 2

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2025-09-03T01:00:00+00:00 - Task ID: 1
-const onHandleSubmit = (e) => { e.preventDefault(); console.log('Form Submitted'); };
+// Update for 2025-09-03T02:00:00+00:00 - Task ID: 2
+const fetchData = async (url) => { const res = await fetch(url); return res.json(); };
 
 export default {};
