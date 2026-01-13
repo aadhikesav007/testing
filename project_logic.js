933ef110c21c6497c29d67eb0bed96379f4dc822
aadhikesav007 <hackathonkrce@gmail.com>
2026-01-13 09:00:00 +0000

    Refactor: implement logic module 9

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2026-01-13T08:00:00+00:00 - Task ID: 8
-const debounce = (fn, ms) => { let timeout; return () => { clearTimeout(timeout); timeout = setTimeout(fn, ms); }; };
+// Update for 2026-01-13T09:00:00+00:00 - Task ID: 9
+const getLocalStorage = (key) => JSON.parse(localStorage.getItem(key));
 
 export default {};
