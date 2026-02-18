5252d981176788be4d726e39ed0da8a77c251680
aadhikesav007 <hackathonkrce@gmail.com>
2026-02-18 03:00:00 +0000

    Refactor: implement logic module 3

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2026-02-18T02:00:00+00:00 - Task ID: 2
-const formatCurrency = (amount) => new Intl.NumberFormat('en-IN').format(amount);
+// Update for 2026-02-18T03:00:00+00:00 - Task ID: 3
+const validateEmail = (email) => /^[^s@]+@[^s@]+.[^s@]+$/.test(email);
 
 export default {};
