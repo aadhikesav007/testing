1f8ce686f19da09c743cf99825bbb09936a6eafb
aadhikesav007 <hackathonkrce@gmail.com>
2026-02-23 09:00:00 +0000

    Refactor: implement logic module 9

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2026-02-23T08:00:00+00:00 - Task ID: 8
-const formatCurrency = (amount) => new Intl.NumberFormat('en-IN').format(amount);
+// Update for 2026-02-23T09:00:00+00:00 - Task ID: 9
+const toggleModal = () => setIsOpen(prev => !prev);
 
 export default {};
