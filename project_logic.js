e4f406768e0ba22b4d7387320b21a1d61a1e11db
aadhikesav007 <hackathonkrce@gmail.com>
2026-06-25 05:00:00 +0000

    Refactor: implement logic module 5

--- project_logic.js
+++ project_logic.js
@@ -1,4 +1,4 @@
-// Update for 2026-06-25T04:00:00+00:00 - Task ID: 4
-const calculateTotal = (cart) => cart.reduce((acc, item) => acc + item.price, 0);
+// Update for 2026-06-25T05:00:00+00:00 - Task ID: 5
+const filterActiveUsers = (users) => users.filter(user => user.isActive);
 
 export default {};
